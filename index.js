9c4660aba07850d215926b7fb7107e62cfafe7ce
cerramaximiliano <cerramaximiliano@gmail.com>
2023-12-03 14:39:57 -0300

    backend 3/5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
@@ -9,18 +10,38 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get('/api/shorturl', (req, res) => {
-  res.json({
-    error: "Invalid URL"
-  })
+function isValidUrl(string) {
+  try {
+    new URL(string);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+
+app.post('/api/shorturl', (req, res) => {
+  const {url} = req.body
+  console.log(url)
+  console.log(isValidUrl(url))
+  if (isValidUrl(url)) {
+    res.json({
+      original_url: url,
+      short_url: Math.random()
+    })
+  }else{
+    res.json({
+      error: "Invalid URL"
+    })
+  }
+
 });
-
-
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
